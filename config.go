17f6408ef43b25a56779a4770b9996c504f73949
Florian Plattner <me@florianplattner.de>
2016-11-10 01:16:44 +0100

    implements a simple config system

---
+++ config.go
@@ -0,0 +1,168 @@
+package config
+
+import (
+	"strings"
+	"errors"
+	"fmt"
+	"strconv"
+	"syscall"
+)
+
+var ACCEPTABLE_TRUETHY []string = []string{"yes", "1", "true", "y"}
+var ACCEPTABLE_FALSY []string = []string{"no", "0", "false", "n"}
+
+/**
+ * for a list of path components load the value to be set that path or an error if the path cannot be resolved
+ */
+type Loader func(pathElements []string) (interface{}, error)
+
+type Config struct {
+	loader Loader
+	pathElements []string
+	errors []error
+}
+
+func (c *Config) GetErrors() []error {
+	return c.errors
+}
+
+func (c *Config) ChildConfig(name string) (*Config) {
+	return &Config{
+		loader: c.loader,
+		pathElements: append(c.pathElements, name),
+	}
+}
+
+func (c *Config) GetString(name string) (string, error) {
+	value, loadErr := c.loader(append(c.pathElements, name))
+	if loadErr != nil {
+		c.errors = append(c.errors, loadErr)
+		return "", loadErr
+	}
+
+	switch t := value.(type) {
+	case string:
+		return t, nil
+	default:
+		err := errors.New(fmt.Sprintf("value is not a string for name %s", name))
+		c.errors = append(c.errors, err)
+		return "", err
+	}
+}
+
+func (c *Config) GetBool(name string) (bool, error) {
+	value, loadErr := c.loader(append(c.pathElements, name))
+	if loadErr != nil {
+		c.errors = append(c.errors, loadErr)
+		return false, loadErr
+	}
+
+	switch t := value.(type) {
+	case string:
+		if inArray(value.(string), ACCEPTABLE_TRUETHY) {
+			return true, nil
+		} else if inArray(value.(string), ACCEPTABLE_FALSY) {
+			return false, nil
+		}
+	case bool:
+		return t, nil
+	}
+
+	err := errors.New(fmt.Sprintf("value is not a valid boolean or parseable string for name %s", name))
+	c.errors = append(c.errors, err)
+	return false, err
+}
+
+func (c *Config) GetInt(name string) (int, error) {
+	value, loadErr := c.loader(append(c.pathElements, name))
+	if loadErr != nil {
+		c.errors = append(c.errors, loadErr)
+		return 0, loadErr
+	}
+
+	switch t := value.(type) {
+	case string:
+		intval, parseErr := strconv.Atoi(value.(string))
+		if parseErr != nil {
+			c.errors = append(c.errors, parseErr)
+			return 0, parseErr
+		}
+		return intval, nil
+	case int:
+		return t, nil
+	}
+
+	err := errors.New("value is not an int or parseable string for name " + name)
+	c.errors = append(c.errors, err)
+	return 0, err
+}
+
+func (c *Config) GetFloat(name string) (float64, error) {
+	value, loadErr := c.loader(append(c.pathElements, name))
+	if loadErr != nil {
+		c.errors = append(c.errors, loadErr)
+		return 0, loadErr
+	}
+
+	switch t := value.(type) {
+	case string:
+		floatVal, parseErr := strconv.ParseFloat(t, 64)
+		if parseErr != nil {
+			c.errors = append(c.errors, parseErr)
+			return 0, parseErr
+		}
+		return floatVal, nil
+	case float32:
+		return float64(t), nil
+	case float64:
+		return t, nil
+	case int:
+		return float64(t), nil
+	}
+
+	err := errors.New("value is not a compatible float (32, 64), int or parseable string for name " + name)
+	c.errors = append(c.errors, err)
+	return 0, err
+}
+
+func NewConfig(applicationName string, loader Loader) *Config {
+	return &Config{loader: loader, pathElements: []string{applicationName}}
+}
+
+func NewEnvironmentLoader() Loader {
+	return func(pathElements []string) (interface{}, error) {
+		variableName := strings.ToUpper(strings.Join(pathElements, "_"))
+		value, exists := syscall.Getenv(variableName)
+		if !exists {
+			return "", errors.New(fmt.Sprintf("environment variable %s does not exist", variableName))
+		}
+
+		return value, nil
+	}
+}
+
+//func NewJsonLoader(filePath string) (Loader, error) {
+//	fileContents, loadErr := ioutil.ReadFile(filePath)
+//	if loadErr != nil {
+//		return nil, loadErr
+//	}
+//
+//	parsedContents := new(interface{})
+//	parseErr := json.Unmarshal(fileContents, parsedContents)
+//	if parseErr != nil {
+//		return nil, parseErr
+//	}
+//
+//	return func(pathElements []string) (interface{}, error) {
+//
+//	}
+//}
+
+func inArray(needle string, haystack []string) bool {
+	for _, v := range haystack {
+		if v == strings.ToLower(needle) {
+			return true
+		}
+	}
+	return false
+}
---
+++ config_test.go
@@ -0,0 +1,471 @@
+package config_test
+
+import (
+	"testing"
+	"github.com/flowpl/config"
+	"errors"
+	"reflect"
+	"os"
+)
+
+var getBooleanValidValues = []struct{
+	input string
+	expected bool
+} {
+	{input: "true", expected: true},
+	{input: "TRUE", expected: true},
+	{input: "1", expected: true},
+	{input: "yes", expected: true},
+	{input: "YES", expected: true},
+	{input: "y", expected: true},
+	{input: "Y", expected: true},
+
+	{input: "false", expected: false},
+	{input: "FALSE", expected: false},
+	{input: "0", expected: false},
+	{input: "no", expected: false},
+	{input: "NO", expected: false},
+	{input: "n", expected: false},
+	{input: "N", expected: false},
+}
+
+func TestConfig_GetBool_ShouldReturnValidValuesForValidInputs(t *testing.T) {
+	for _, testData := range getBooleanValidValues {
+		c := config.NewConfig("app", func(name []string) (interface{}, error) {
+			return testData.input, nil
+		})
+
+		result, _ := c.GetBool("someName")
+
+		if result != testData.expected {
+			t.Errorf("expected bool result for %s to be %t. Actual: %t", testData.input, testData.expected, result)
+		}
+	}
+}
+
+func TestConfig_GetBool_ShouldReturnAnErrorIfTheLoaderReturnsAnError(t *testing.T) {
+	c := config.NewConfig("app", func(name []string) (interface{}, error) {
+		return "", errors.New("testerror")
+	})
+
+	_, err := c.GetBool("someName")
+
+	if err == nil {
+		t.Error("expected an error to be returned, but it wasn't")
+	}
+
+	if len(c.GetErrors()) == 0 {
+		t.Error("expected error to be appended to the Config error list")
+	}
+}
+
+func TestConfig_GetBool_ShouldReturnAnErrorIfTheConfigStringCannotBeParsedToBool(t *testing.T) {
+	c := config.NewConfig("app", func(name []string) (interface{}, error) {
+		return "NotABool", nil
+	})
+
+	_, err := c.GetBool("someName")
+
+	if err == nil {
+		t.Error("expected an error to be returned, but it wasn't")
+	}
+
+	if len(c.GetErrors()) == 0 {
+		t.Error("expected error to be appended to the Config error list")
+	}
+}
+
+func TestConfig_GetBool_ShouldReturnTheValueDirectlyIfLoaderReturnsBool(t *testing.T) {
+	c := config.NewConfig("app", func(name []string) (interface{}, error) {
+		return true, nil
+	})
+
+	result, _ := c.GetBool("someName")
+
+	if result != true {
+		t.Errorf("expected bool result for true to be true. Actual: %t", result)
+	}
+}
+
+func TestConfig_GetBool_ShouldReturnAnErrorIfLoaderNeitherReturnsStringNorBool(t *testing.T) {
+	c := config.NewConfig("app", func(name []string) (interface{}, error) {
+		return 65, nil
+	})
+
+	_, err := c.GetBool("someName")
+
+	if err == nil {
+		t.Error("expected an error to be returned, but it wasn't")
+	}
+
+	if len(c.GetErrors()) == 0 {
+		t.Error("expected error to be appended to the Config error list")
+	}
+}
+
+func TestConfig_GetBool_ShouldPassTheCorrectPathComponentsToTheLoader(t *testing.T) {
+	var passedPath []string
+	c := config.NewConfig("app", func(name []string) (interface{}, error) {
+		passedPath = name
+		return true, nil
+	})
+
+	c.GetBool("someName")
+
+	if !reflect.DeepEqual(passedPath, []string{"app", "someName"}) {
+		t.Error("expected path to be []string{\"app\",\"someName\"}, but it wasn't")
+	}
+}
+
+func TestConfig_GetString_ShouldReturnAValueDirectlyFromLoader(t *testing.T) {
+	const testValue = "someValue"
+	c := config.NewConfig("app", func(name []string) (interface{}, error) {
+		return testValue, nil
+	})
+
+	result, _ := c.GetString("someName")
+
+	if result != testValue {
+		t.Errorf("expected result to be \"%s\", actual: \"%s\"", testValue, result)
+	}
+}
+
+func TestConfig_GetString_ShouldReturnAnErrorIfLoaderReturnsAnError(t *testing.T) {
+	c := config.NewConfig("app", func(name []string) (interface{}, error) {
+		return "", errors.New("testerror")
+	})
+
+	_, err := c.GetString("someName")
+
+	if err == nil {
+		t.Error("expected an error to be returned, but it wasn't")
+	}
+
+	if len(c.GetErrors()) == 0 {
+		t.Error("expected error to be appended to the Config error list")
+	}
+}
+
+func TestConfig_GetString_ShouldReturnAnErrorIfLoaderDoesNotReturnString(t *testing.T) {
+	c := config.NewConfig("app", func(name []string) (interface{}, error) {
+		return 65, nil
+	})
+
+	_, err := c.GetString("someName")
+
+	if err == nil {
+		t.Error("expected an error to be returned, but it wasn't")
+	}
+
+	if len(c.GetErrors()) == 0 {
+		t.Error("expected error to be appended to the Config error list")
+	}
+}
+
+func TestConfig_GetString_ShouldPassTheCorrectPathComponentsToTheLoader(t *testing.T) {
+	var passedPath []string
+	c := config.NewConfig("app", func(name []string) (interface{}, error) {
+		passedPath = name
+		return "string", nil
+	})
+
+	c.GetString("someName")
+
+	if !reflect.DeepEqual(passedPath, []string{"app", "someName"}) {
+		t.Error("expected path to be []string{\"app\",\"someName\"}, but it wasn't")
+	}
+}
+
+var getIntValidStrings = []struct{
+	input string
+	expected int
+} {
+	{input: "1", expected: 1},
+	{input: "0", expected: 0},
+	{input: "-1", expected: -1},
+	{input: "2000000000", expected: 2000000000},
+	{input: "-2000000000", expected:-2000000000},
+}
+
+func TestConfig_GetInt_ShouldReturnAValidValueForValidInputString(t *testing.T) {
+	for _, testData := range getIntValidStrings {
+		c := config.NewConfig("app", func(name []string) (interface{}, error) {
+			return testData.input, nil
+		})
+
+		result, _ := c.GetInt("someName")
+
+		if result != testData.expected {
+			t.Errorf("expected result for %s to be %d, actual %d", testData.input, testData.expected, result)
+		}
+	}
+}
+
+func TestConfig_GetInt_ShouldReturnAValueDirectlyFromLoaderIfItsInt(t *testing.T) {
+	const testValue = 32
+	c := config.NewConfig("app", func(name []string) (interface{}, error) {
+		return testValue, nil
+	})
+
+	result, _ := c.GetInt("someName")
+
+	if result != testValue {
+		t.Errorf("expected result for %d to be %d, actual %d", testValue, testValue, result)
+	}
+}
+
+func TestConfig_GetInt_ShouldReturnErrorIfLoaderReturnsError(t *testing.T) {
+	c := config.NewConfig("app", func(name []string) (interface{}, error) {
+		return "", errors.New("testerror")
+	})
+
+	_, err := c.GetInt("someName")
+
+	if err == nil {
+		t.Error("expected an error to be returned, but it wasn't")
+	}
+
+	if len(c.GetErrors()) == 0 {
+		t.Error("expected error to be appended to the Config error list")
+	}
+}
+
+func TestConfig_GetInt_ShouldReturnErrorIfLoaderReturnsAnUnsupportedType(t *testing.T) {
+	c := config.NewConfig("app", func(name []string) (interface{}, error) {
+		return true, nil
+	})
+
+	_, err := c.GetInt("someName")
+
+	if err == nil {
+		t.Error("expected an error to be returned, but it wasn't")
+	}
+
+	if len(c.GetErrors()) == 0 {
+		t.Error("expected error to be appended to the Config error list")
+	}
+}
+
+func TestConfig_GetInt_ShouldReturnErrorIfLoaderReturnsUnparseableString(t *testing.T) {
+	c := config.NewConfig("app", func(name []string) (interface{}, error) {
+		return "true", nil
+	})
+
+	_, err := c.GetInt("someName")
+
+	if err == nil {
+		t.Error("expected an error to be returned, but it wasn't")
+	}
+
+	if len(c.GetErrors()) == 0 {
+		t.Error("expected error to be appended to the Config error list")
+	}
+}
+
+func TestConfig_GetInt_ShouldPassTheCorrectPathComponentsToTheLoader(t *testing.T) {
+	var passedPath []string
+	c := config.NewConfig("app", func(name []string) (interface{}, error) {
+		passedPath = name
+		return 32, nil
+	})
+
+	c.GetInt("someName")
+
+	if !reflect.DeepEqual(passedPath, []string{"app", "someName"}) {
+		t.Error("expected path to be []string{\"app\",\"someName\"}, but it wasn't")
+	}
+}
+
+var getFloatValidStrings = []struct{
+	input string
+	expected float64
+} {
+	{input: "1.0", expected: 1},
+	{input: "0", expected: 0},
+	{input: "-1.0", expected: -1},
+	{input: "2000000000", expected: 2000000000},
+	{input: "-2000000000", expected:-2000000000},
+	{input: "15.98754379", expected:15.98754379},
+	{input: "-15.98754379", expected:-15.98754379},
+}
+
+func TestConfig_GetFloat_ShouldReturnAValidValueForValidInputString(t *testing.T) {
+	for _, testData := range getFloatValidStrings {
+		c := config.NewConfig("app", func(name []string) (interface{}, error) {
+			return testData.input, nil
+		})
+
+		result, _ := c.GetFloat("someName")
+
+		if result != testData.expected {
+			t.Errorf("expected result for %s to be %d, actual %d", testData.input, testData.expected, result)
+		}
+	}
+}
+
+func TestConfig_GetFloat_ShouldReturnAValueDirectlyFromLoaderIfItsFloat64(t *testing.T) {
+	const testValue float64 = 4.5
+	c := config.NewConfig("app", func(name []string) (interface{}, error) {
+		return testValue, nil
+	})
+
+	result, _ := c.GetFloat("someName")
+
+	if result != testValue {
+		t.Errorf("expected result for %d to be %d, actual %d", testValue, testValue, result)
+	}
+}
+
+func TestConfig_GetFloat_ShouldCastToFloat64IfLoaderReturnsInt(t *testing.T) {
+	const testValue int = 4
+	c := config.NewConfig("app", func(name []string) (interface{}, error) {
+		return testValue, nil
+	})
+
+	result, _ := c.GetFloat("someName")
+
+	floatTestValue := float64(testValue)
+	if floatTestValue != result {
+		t.Errorf("expected result for %d to be %d, actual %d", testValue, testValue, result)
+	}
+}
+
+func TestConfig_GetFloat_ShouldCastToFloat64IfLoaderReturnsFloat32(t *testing.T) {
+	const testValue float32 = 4
+	c := config.NewConfig("app", func(name []string) (interface{}, error) {
+		return testValue, nil
+	})
+
+	result, _ := c.GetFloat("someName")
+
+	floatTestValue := float64(testValue)
+	if floatTestValue != result {
+		t.Errorf("expected result for %d to be %d, actual %d", testValue, testValue, result)
+	}
+}
+
+func TestConfig_GetFloat_ShouldReturnErrorIfLoaderReturnsError(t *testing.T) {
+	c := config.NewConfig("app", func(name []string) (interface{}, error) {
+		return "", errors.New("testerror")
+	})
+
+	_, err := c.GetFloat("someName")
+
+	if err == nil {
+		t.Error("expected an error to be returned, but it wasn't")
+	}
+
+	if len(c.GetErrors()) == 0 {
+		t.Error("expected error to be appended to the Config error list")
+	}
+}
+
+func TestConfig_GetFloat_ShouldReturnErrorIfLoaderReturnsAnUnsupportedType(t *testing.T) {
+	c := config.NewConfig("app", func(name []string) (interface{}, error) {
+		return true, nil
+	})
+
+	_, err := c.GetFloat("someName")
+
+	if err == nil {
+		t.Error("expected an error to be returned, but it wasn't")
+	}
+
+	if len(c.GetErrors()) == 0 {
+		t.Error("expected error to be appended to the Config error list")
+	}
+}
+
+func TestConfig_GetFloat_ShouldReturnErrorIfLoaderReturnsUnparseableString(t *testing.T) {
+	c := config.NewConfig("app", func(name []string) (interface{}, error) {
+		return "true", nil
+	})
+
+	_, err := c.GetFloat("someName")
+
+	if err == nil {
+		t.Error("expected an error to be returned, but it wasn't")
+	}
+
+	if len(c.GetErrors()) == 0 {
+		t.Error("expected error to be appended to the Config error list")
+	}
+}
+
+func TestConfig_GetFloat_ShouldPassTheCorrectPathComponentsToTheLoader(t *testing.T) {
+	var passedPath []string
+	c := config.NewConfig("app", func(name []string) (interface{}, error) {
+		passedPath = name
+		return 3.4, nil
+	})
+
+	c.GetFloat("someName")
+
+	if !reflect.DeepEqual(passedPath, []string{"app", "someName"}) {
+		t.Error("expected path to be []string{\"app\",\"someName\"}, but it wasn't")
+	}
+}
+
+func TestConfig_ShouldPassAFourLevelPathIfGetChildHasBeenCalledTwice(t *testing.T) {
+	var passedPath []string
+	c := config.NewConfig("app", func(name []string) (interface{}, error) {
+		passedPath = name
+		return "0", nil
+	})
+	child1 := c.ChildConfig("one")
+	child2 := child1.ChildConfig("two")
+
+	child2.GetBool("someName")
+	if !reflect.DeepEqual(passedPath, []string{"app", "one", "two", "someName"}) {
+		t.Error("expected path to be []string{\"app\",\"one\",\"two\",\"someName\"} in GetBool(), but it wasn't")
+	}
+
+	child2.GetString("someName")
+	if !reflect.DeepEqual(passedPath, []string{"app", "one", "two", "someName"}) {
+		t.Error("expected path to be []string{\"app\",\"one\",\"two\",\"someName\"} in GetString(), but it wasn't")
+	}
+
+	child2.GetInt("someName")
+	if !reflect.DeepEqual(passedPath, []string{"app", "one", "two", "someName"}) {
+		t.Error("expected path to be []string{\"app\",\"one\",\"two\",\"someName\"} in GetInt(), but it wasn't")
+	}
+
+	child2.GetFloat("someName")
+	if !reflect.DeepEqual(passedPath, []string{"app", "one", "two", "someName"}) {
+		t.Error("expected path to be []string{\"app\",\"one\",\"two\",\"someName\"} in GetFloat(), but it wasn't")
+	}
+}
+
+func TestConfig_ShouldPassATwoLevelPathIfChildHasBeenCalledOnceButConfigLookupIsOnTheOriginalConfig(t *testing.T) {
+	var passedPath []string
+	c := config.NewConfig("app", func(name []string) (interface{}, error) {
+		passedPath = name
+		return "0", nil
+	})
+	child1 := c.ChildConfig("one")
+
+	child1.GetBool("someName")
+	if !reflect.DeepEqual(passedPath, []string{"app", "one", "someName"}) {
+		t.Error("expected path to be []string{\"app\",\"one\",\"someName\"} in GetBool(), but it wasn't")
+	}
+
+	c.GetBool("someName")
+	if !reflect.DeepEqual(passedPath, []string{"app", "someName"}) {
+		t.Error("expected path to be []string{\"app\",\"someName\"} in GetBool(), but it wasn't")
+	}
+}
+
+func TestConfig_ShouldLoadAnActualVariableFromTheEnvironment(t *testing.T) {
+	os.Setenv("APP_ONE_VALUE", "success")
+	c := config.NewConfig("app", config.NewEnvironmentLoader())
+	child1 := c.ChildConfig("one")
+
+	result, err := child1.GetString("value")
+	if err != nil {
+		t.Error("an error was not expected to be returned but it was")
+	}
+
+	if result != "success" {
+		t.Errorf("expected value for name %s to be %s, actual %s", "value", "success", result)
+	}
+}
